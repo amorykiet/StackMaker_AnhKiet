f64102550628bd2e80874d2b17529d174fcb5206
noha <amorykiet@gmail.com>
2024-08-14 21:07:18 +0700

    update level script

--- Assets/_Game/Scripts/Level.cs
+++ Assets/_Game/Scripts/Level.cs
@@ -7,36 +7,11 @@ using static Player;
 public class Level : MonoBehaviour
 {
     [SerializeField] public Transform startPos;
-    
-    public Player player;
-
-    private int score;
-    private bool completed = false;
 
     private void Start()
     {
         OnInit();
     }
 
-    public void OnInit()
-    {
-        score = 0;
-        completed = false;
-    }
-
-    public void Complete()
-    {
-        completed = true;
-    }
-
-    public void SetScore(int score)
-    {
-        this.score = score;
-    }
-
-    public bool IsComplete()
-    {
-        return completed;
-    }
-
+    public void OnInit() { }
 }
